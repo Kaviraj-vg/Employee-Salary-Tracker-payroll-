36f260aa457c45ce8ca33992f6927a26e65bb043
Kaviraj <kavirajvg18@gmail.com>
2024-12-06 02:16:56 +0530

    Update sql_schema.sql

--- sql_schema.sql
+++ sql_schema.sql
@@ -9,21 +9,6 @@ CREATE TABLE Employee (
     department VARCHAR(50)
 );
 
-
-CREATE TABLE payslips (
-    payslip_id INT AUTO_INCREMENT PRIMARY KEY,
-    employee_id INT NOT NULL,
-    month VARCHAR(20) NOT NULL,
-    basic_pay DOUBLE NOT NULL,
-    hra DOUBLE NOT NULL,
-    da DOUBLE NOT NULL,
-    other_allowances DOUBLE NOT NULL,
-    deductions DOUBLE NOT NULL,
-    gross_salary DOUBLE NOT NULL,
-    net_salary DOUBLE NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES Employee(id)
-);
-
 CREATE TABLE payslips (
     payslip_id INT AUTO_INCREMENT PRIMARY KEY,
     employee_id INT NOT NULL,
